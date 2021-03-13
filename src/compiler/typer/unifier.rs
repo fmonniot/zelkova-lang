5aaecfc6f4824f4987e7c071fe681ca8fba43b63
François Monniot <francoismonniot@gmail.com>
2021-03-13 07:45:02 -0800

    unifier unit tests

--- src/compiler/typer/unifier.rs
+++ src/compiler/typer/unifier.rs
@@ -50,7 +50,7 @@ fn unify_one_constraint(Constraint(a, b): &Constraint) -> Substitution {
         }
         (Type::Variable(tvar), tpe) => unify_variable(tvar, tpe),
         (tpe, Type::Variable(tvar)) => unify_variable(tvar, tpe),
-        (a, b) => panic!("Cannot unify {:?} with {:?}", a, b),
+        (a, b) => panic!("Cannot unify {:?} with {:?}", a, b), // TODO Return Result instead
     }
 }
 
@@ -66,7 +66,7 @@ fn unify_variable(tvar: &TypeVariable, tpe: &Type) -> Substitution {
         }
         _ => {
             if occurs(tvar, tpe) {
-                panic!("circular use: {:?} occurs in {:?}", tvar, tpe)
+                panic!("circular use: {:?} occurs in {:?}", tvar, tpe) // TODO Return Result instead
             } else {
                 Substitution::one(tvar.clone(), tpe.clone())
             }
@@ -82,5 +82,94 @@ fn occurs(tvar: &TypeVariable, tpe: &Type) -> bool {
         } => occurs(tvar, param_tpe) || occurs(tvar, return_tpe),
         Type::Variable(tvar2) => tvar == tvar2,
         _ => false,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::compiler::typer::*;
+
+    #[test]
+    fn unifies_ints() {
+        let mut constraints = HashSet::new();
+        constraints.insert(Constraint(
+            Type::Literal(TypeLiteral::Int),
+            Type::Literal(TypeLiteral::Int),
+        ));
+
+        assert_eq!(unify(constraints), Substitution::empty());
+    }
+
+    #[test]
+    fn unifies_bools() {
+        let mut constraints = HashSet::new();
+        constraints.insert(Constraint(
+            Type::Literal(TypeLiteral::Bool),
+            Type::Literal(TypeLiteral::Bool),
+        ));
+
+        assert_eq!(unify(constraints), Substitution::empty());
+    }
+
+    #[test]
+    fn unifies_functions() {
+        let fun = Type::Fun {
+            param_tpe: Box::new(Type::Literal(TypeLiteral::Bool)),
+            return_tpe: Box::new(Type::Literal(TypeLiteral::Bool)),
+        };
+        let mut constraints = HashSet::new();
+        constraints.insert(Constraint(fun.clone(), fun.clone()));
+
+        assert_eq!(unify(constraints), Substitution::empty());
+    }
+
+    #[test]
+    fn unifies_variables() {
+        let tvar1 = TypeVariable { id: 1 };
+        let t1 = Type::Variable(tvar1.clone());
+        let t2 = Type::Variable(TypeVariable { id: 2 });
+
+        let mut constraints = HashSet::new();
+        constraints.insert(Constraint(t1, t2.clone()));
+
+        assert_eq!(unify(constraints), Substitution::one(tvar1, t2));
+    }
+
+    #[test]
+    fn unifies_variable_with_literal() {
+        let tvar1 = TypeVariable { id: 1 };
+        let t1 = Type::Variable(tvar1.clone());
+        let t2 = Type::Literal(TypeLiteral::Int);
+
+        let mut constraints = HashSet::new();
+        constraints.insert(Constraint(t1, t2.clone()));
+
+        assert_eq!(unify(constraints), Substitution::one(tvar1, t2));
+    }
+
+    #[test]
+    fn unifies_variables_in_functions() {
+        let tvar1 = TypeVariable { id: 1 };
+        let tvar2 = TypeVariable { id: 2 };
+
+        let mut constraints = HashSet::new();
+        constraints.insert(Constraint(
+            // tvar1 -> bool
+            Type::Fun {
+                param_tpe: Box::new(Type::Variable(tvar1.clone())),
+                return_tpe: Box::new(Type::Literal(TypeLiteral::Bool)),
+            },
+            // int -> tvar2
+            Type::Fun {
+                param_tpe: Box::new(Type::Literal(TypeLiteral::Int)),
+                return_tpe: Box::new(Type::Variable(tvar2.clone())),
+            },
+        ));
+
+        let sub = Substitution::one(tvar2, Type::Literal(TypeLiteral::Bool))
+            .merge(Substitution::one(tvar1, Type::Literal(TypeLiteral::Int)));
+
+        assert_eq!(unify(constraints), sub);
     }
 }
