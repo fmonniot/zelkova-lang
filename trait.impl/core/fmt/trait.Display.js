2d904976cec8be1aa95fd791ce29a89b630f9bf9
fmonniot <fmonniot@users.noreply.github.com>
2025-10-17 05:34:39 +0000

    deploy: 6ac6b4e4bc8f097fbdd88ceea479417789b10a8f

---
+++ trait.impl/core/fmt/trait.Display.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["zelkova_lang",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"zelkova_lang/compiler/name/struct.Name.html\" title=\"struct zelkova_lang::compiler::name::Name\">Name</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[301]}
---
+++ trait.impl/core/ops/arith/trait.Add.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["zelkova_lang",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.90.0/std/primitive.u32.html\">u32</a>&gt; for <a class=\"struct\" href=\"zelkova_lang/compiler/position/struct.BytePos.html\" title=\"struct zelkova_lang::compiler::position::BytePos\">BytePos</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[422]}
